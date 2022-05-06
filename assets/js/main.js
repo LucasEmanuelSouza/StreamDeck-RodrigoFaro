fd1304e75211c1f7190c29b329124a2a7432482a
Lucas Emanuel <86693709+LucasEmanuelSouza@users.noreply.github.com>
2022-05-06 01:32:54 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,119 @@
+//Dança Gatinho
+const botaoGatinho = document.querySelector('#gatinho')
+
+botaoGatinho.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-gatinho')
+    audio.play()
+})
+
+//Cavalo
+const botaoCavalo = document.querySelector('#cavalo')
+
+botaoCavalo.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-cavalo')
+    audio.play()
+})
+
+//Chega
+const botaoChega = document.querySelector('#chega')
+
+botaoChega.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-chega')
+    audio.play()
+})
+
+//Demais
+const botaoDemais = document.querySelector('#demais')
+
+botaoDemais.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-demais')
+    audio.play()
+})
+
+//É brincadeira hein
+const botaoBrincadeira = document.querySelector('#brincadeira')
+
+botaoBrincadeira.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-brincadeira')
+    audio.play()
+})
+
+//Ele gosta
+const botaoEleGosta = document.querySelector('#gosta')
+
+botaoEleGosta.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-gosta')
+    audio.play()
+})
+
+//Não
+const botaoNao = document.querySelector('#nao')
+
+botaoNao.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-nao')
+    audio.play()
+})
+
+//Pare
+const botaoPare = document.querySelector('#pare')
+
+botaoPare.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-pare')
+    audio.play()
+})
+
+//Risada
+const botaoRisada = document.querySelector('#risada')
+
+botaoRisada.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-risada')
+    audio.play()
+})
+
+//Falha
+const botaoFalha = document.querySelector('#falha')
+
+botaoFalha.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-falha')
+    audio.play()
+})
+
+//Barulho estranho
+const botaoEstranho = document.querySelector('#estranho')
+
+botaoEstranho.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-estranho')
+    audio.play()
+})
+
+//Tome
+const botaoTome = document.querySelector('#tome')
+
+botaoTome.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-tome')
+    audio.play()
+})
+
+//Ui
+const botaoUi = document.querySelector('#ui')
+
+botaoUi.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-ui')
+    audio.play()
+})
+
+//Xi
+const botaoXi = document.querySelector('#xi')
+
+botaoXi.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-xi')
+    audio.play()
+})
+
+//Ejetado
+const botaoEjetado = document.querySelector('#ejetado')
+
+botaoEjetado.addEventListener('click',function(){
+    const audio = document.querySelector('.audio-ejetado')
+    audio.play()
+})
